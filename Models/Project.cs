5b571577e4195ac91962a7f2cb63c19e223e71c9
RamaAlfareza67 <42989726+RamaAlfareza67@users.noreply.github.com>
2024-07-18 15:29:01 +0700

    Update Project.cs

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,22 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace project.Models
+﻿namespace mystap.Models
 {
     public class Project
     {
         public long id { get; set; }
         public string? projectNo { get; set; }
-        public string? description { get; set; } 
+        public string? description { get; set; }
         public string? revision { get; set; }
         public string? month { get; set; }
         public string? year { get; set; }
         public string? active { get; set; }
-        public int? delete { get; set; }
-        public int? update { get; set; }
+        public int? deleted { get; set; }
+        public int? updated { get; set; }
         public DateTime? tglTA { get; set; }
         public DateTime? tglSelesaiTA { get; set; }
-        public long? deleteBy { get; set; }
-        public long? createdBy { get; set; }
+        public int? deleteBy { get; set; }
+        public int? createdBy { get; set; }
         public DateTime? createdDate { get; set; }
         public DateTime? lastModify { get; set; }
         public long? modifyBy { get; set; }
