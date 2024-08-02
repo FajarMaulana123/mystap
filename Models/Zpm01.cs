7fb43e0e73f5765288509977f48d06829be22336
RamaAlfareza67 <42989726+RamaAlfareza67@users.noreply.github.com>
2024-08-02 15:05:26 +0700

    Create Zpm01.cs

---
+++ Models/Zpm01.cs
@@ -0,0 +1,46 @@
+﻿namespace mystap.Models
+{
+    public class Zpm01
+    {
+        public long id { get; set; }
+        public string? no_order { get; set; }
+        public string? revision { get; set; }
+        public string? reserv_no { get; set; }
+        public string? material { get; set; }
+        public string? material_description { get; set; }
+        public string? itm { get; set; }
+        public string? sloc { get; set; }
+        public string? pg { get; set; }
+        public string? ict { get; set; }
+        public string? del { get; set; }
+        public string? recipient { get; set; }
+        public string? unloading_point { get; set; }
+        public string? fls { get; set; }
+        public string? diff_qty { get; set; }
+        public string? cost_ctrs { get; set; }
+        public string? gl_acct { get; set; }
+        public string? bun { get; set; }
+        public string? reqmt_date { get; set; }
+        public string? reqmt_qty { get; set; }
+        public string? qty_f_avail_check { get; set; }
+        public string? qty_withdrawn { get; set; }
+        public string? price { get; set; }
+        public string? per { get; set; }
+        public string? crcy { get; set; }
+        public string? qty_res { get; set; }
+        public string? pr { get; set; }
+        public string? itm_pr { get; set; }
+        public string? qty_pr { get; set; }
+        public DateTime? req_date { get; set; }
+        public string? buyer { get; set; }
+        public string? status_pr { get; set; }
+        public string? doc_pr { get; set; }
+        public int? dt_ta { get; set; }
+        public int? dt_iv { get; set; }
+        public int? dt_purch { get; set; }
+        public int? dt_status_pengadaan { get; set; }
+        public string? status_pengadaan { get; set; }
+        public string? keterangan { get; set; }
+        public int? prognosa_matl { get; set; }
+    }
+}
